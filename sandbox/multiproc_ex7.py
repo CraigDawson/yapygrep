515ac27b61cbfcc162314107fd03c5b7e30bdaf7
Craig Dawson <craig.dawson@gmail.com>
2017-10-22 17:40:39 -0400

    Initial check-in of file to test multiprocessor algorithm.

---
+++ sandbox/multiproc_ex7.py
@@ -0,0 +1,54 @@
+# File to test multiprocessor algorithm
+# Has simple dir and file functions
+
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import current_process, cpu_count
+from time import time
+import os
+
+
+def doDir(dir):
+    """
+    Processes a directory
+    :param dir: single directory to process
+    :return: tuple with list of dirs and list of files
+    """
+    files = []
+    dirs = []
+    for entry in os.listdir(dir):
+        if os.path.isfile(dir + '/' + entry):
+            files.append(entry)
+        elif os.path.isdir(dir + '/' + entry):
+            dirs.append(dir + '/' + entry)
+        else:
+            print('{} is not a file or directory.'.format(entry))
+    return (dirs, files)
+
+
+def printDirs(dir):
+    """
+    Test function for doDir()
+    :param dir: directory to process
+    :return: None
+    """
+    (dirs, files) = doDir(dir)
+    for d in dirs:
+        print('d - {}'.format(d))
+        printDirs(d)
+    for f in files:
+        print('f - {}/{}'.format(dir, f))
+
+
+
+def doFiles(files):
+    """
+    Processes list files
+    :param files: list of files to process
+    :return: None
+    """
+    pass
+
+
+if __name__ == '__main__':
+    printDirs('.')
+
