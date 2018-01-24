abfe79ed3bb14117f0aee9684ea26d144af80c7f
Craig Dawson <craig.dawson@gmail.com>
2018-01-24 17:36:02 -0500

    clean up - still not matching local files

--- gui/yapgrep_main_gui.py
+++ gui/yapgrep_main_gui.py
@@ -7,6 +7,7 @@ import os
 from os.path import join, getsize
 import glob
 import regex
+import time
 
 
 class YapgrepGuiProgram(Ui_MainWindow):
@@ -24,54 +25,23 @@ class YapgrepGuiProgram(Ui_MainWindow):
         self.pushButton.clicked.connect(self.search)
 
     def search(self):
+        self.files = 0
+        self.matches = 0
         self.statusbar.showMessage('Searching . . .')
         self.plainTextEdit.clear()
         directory = self.lineEdit.text()
-        print('Our Directory from user:', directory)
+        print('Directory from user:', directory)
         try:
             self.wd(directory,regex.compile("for"))
         except:
             print ("some error occurred!", sys.exc_info())
-        '''for d in glob.glob(directory):
-            self.dbg('d', d)
-            if os.path.isdir(d):
-                self.walkDir(d,"for")
-            else:
-                self.plainTextEdit.appendPlainText(d)
-                QApplication.instance().processEvents()
-        '''
-        print("done with walkDir")
-        self.statusbar.showMessage('Searching completed.')
-
         
-        '''   def dirWalk(self, directory):
-        print('dir: ' + directory)
-        try:
-            for root, dirs, files in os.walk(directory):
-                print(root)
-                # print('Dirs: ')
-                # print(dirs)
-                # print('Files: {}'.format(files))
-                # print(root, "consumes", end=" ")
-                # print("bytes in", len(files), "non-directory files")
-                # print(sum(getsize(join(root, name)) for name in files), end=" ")
-
-                for name in files:
-                    self.plainTextEdit.appendPlainText(join(root, name))
-                    QApplication.instance().processEvents()
-                    
-                if '.git' in dirs:
-                    dirs.remove('.git')  # don't visit git directories
-                    
-        except:
-            print ("some error occurred!", sys.exc_info()[0])
-        '''
+        self.statusbar.showMessage('Searching completed.')
 
     def dbg(self, prefix, item):
         print(prefix + ':', item)
         
     def wd(self, fileSpec, pattern):
-        print('walkDir',fileSpec, 'pattern', pattern)
         fs = os.path.expanduser(fileSpec)
         self.dbg('fs/expanduser', fs)
 
@@ -92,7 +62,6 @@ class YapgrepGuiProgram(Ui_MainWindow):
         self.dbg('path', path)
 
         for p in glob.iglob(fs, recursive=True):
-            # print('p = ', p)
             if os.path.isfile(p):
                 buf = self.grepFile(p, pattern)
                 if buf:
@@ -100,6 +69,7 @@ class YapgrepGuiProgram(Ui_MainWindow):
                     print("".join(buf))
 
         self.dbg('Final fs', fs)
+        print('Files searched: {}, Matches found: {}'.format(self.files, self.matches))
 
 
     def grepFile(self, fileName, pattern):
@@ -108,11 +78,12 @@ class YapgrepGuiProgram(Ui_MainWindow):
         buf = []
         with open(fileName, 'r') as f:
             try:
-                # print("filename", fileName)
+                self.files += 1
+                self.statusbar.showMessage(fileName)
+                #time.sleep(1)
                 for i, line in enumerate(f):
-                    #print("i, line", i, line)
                     if pattern.match(line):
-                        print("  ", i, line, end='')
+                        self.matches += 1
                         buf.append('    {}:{}'.format(i, line))
             except UnicodeDecodeError:
                 pass
